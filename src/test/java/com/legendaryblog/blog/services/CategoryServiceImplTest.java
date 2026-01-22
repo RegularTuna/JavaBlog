fc92985fbf64d041292e2dfd53527a6102c85c04
verytuna <tiagofortuna412@gmail.com>
2026-01-22 19:42:26 +0000

    TEST: Add tests for categoryServiceImpl. (fetch categories, fetch by id, delete)

--- src/test/java/com/legendaryblog/blog/services/CategoryServiceImplTest.java
+++ src/test/java/com/legendaryblog/blog/services/CategoryServiceImplTest.java
@@ -1,21 +1,107 @@
 package com.legendaryblog.blog.services;
 
+import com.legendaryblog.blog.dtos.CategoryDTO;
+import com.legendaryblog.blog.entities.Category;
+import com.legendaryblog.blog.exceptions.ConflictException;
+import com.legendaryblog.blog.exceptions.ResourceNotFoundException;
+import com.legendaryblog.blog.repositories.CategoryRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
+@ExtendWith(MockitoExtension.class)
 class CategoryServiceImplTest {
 
+    @Mock
+    private CategoryRepository categoryRepository;
+
+    @InjectMocks
+    private CategoryServiceImpl categoryService;
+
     @Test
-    void createCategory() {
+    void createCategory_shouldCreateCategory_WhenDataIsValid() {
+        CategoryDTO request = createDTO(null,"Test1");
+
+        when(categoryRepository.findCategoryByName("Test1")).thenReturn(Optional.empty());
+        when(categoryRepository.save(any(Category.class))).thenAnswer(i -> {
+            Category c = i.getArgument(0);
+            c.setId(1);
+            return c;
+        });
+
+        CategoryDTO result = categoryService.createCategory(request);
+
+        assertNotNull(result.getId());
+        assertEquals(1, result.getId());
+        assertEquals("Test1", result.getName());
+        verify(categoryRepository,times(1)).save(any(Category.class));
     }
 
     @Test
-    void fetchCategories() {
+    void createCategory_shouldThrowConflictException_WhenNameAlreadyTaken(){
+        CategoryDTO request = createDTO(null, "Test1");
+        Category existing = createCategory(1, "Test1");
+
+        when(categoryRepository.findCategoryByName("Test1")).thenReturn(Optional.of(existing));
+
+        assertThrows(ConflictException.class, () -> categoryService.createCategory(request));
+
+        verify(categoryRepository, never()).save(any());
+    }
+
+
+    @Test
+    void fetchCategories_shouldReturnListOfCategoryDTO() {
+        Category category1 = createCategory(1, "Test1");
+        Category category2 = createCategory(2, "Test2");
+
+        when(categoryRepository.findAll()).thenReturn(List.of(category1, category2));
+
+        List<CategoryDTO> result = categoryService.fetchCategories();
+
+        assertNotNull(result);
+        assertEquals(2, result.size());
+        assertEquals("Test1", result.get(0).getName());
+        assertEquals("Test2", result.get(1).getName());
+
+        verify(categoryRepository, times(1)).findAll();
     }
 
     @Test
-    void fetchCategoryById() {
+    void fetchCategoryById_shouldReturnCategory_whenExists() {
+        Integer request = 1;
+        Category existing = createCategory(1, "Test1");
+
+        when(categoryRepository.findCategoriesById(1)).thenReturn(Optional.of(existing));
+
+        CategoryDTO result = categoryService.fetchCategoryById(1);
+
+        assertEquals(1, result.getId());
+        assertEquals("Test1", result.getName());
+
+        verify(categoryRepository, times(1)).findCategoriesById(1);
+
+    }
+
+    @Test
+    void fetchCategoryById_shouldThrowResourceNotFoundException_whenCategoryDoesNotExist(){
+        Integer request = 99;
+
+        when(categoryRepository.findCategoriesById(request)).thenReturn(Optional.empty());
+
+        assertThrows(ResourceNotFoundException.class, () -> categoryService.fetchCategoryById(request));
+
+        verify(categoryRepository, times(1)).findCategoriesById(request);
     }
 
     @Test
@@ -23,6 +109,45 @@ class CategoryServiceImplTest {
     }
 
     @Test
-    void deleteCategory() {
+    void deleteCategory_shouldReturnDeletedCategory_whenCategoryExists() {
+        Integer request = 1;
+        Category existing = createCategory(request, "Test1");
+
+        when(categoryRepository.findCategoriesById(request)).thenReturn(Optional.of(existing));
+
+        CategoryDTO result = categoryService.deleteCategory(request);
+
+        assertNotNull(result);
+        assertEquals(request, result.getId());
+        assertEquals(existing.getName(), result.getName());
+
+        verify(categoryRepository, times(1)).deleteById(request);
+    }
+
+    @Test
+    void deleteCategory_shouldThrowResourceNotFoundException_whenCategoryDoesNotExist(){
+        Integer request = 1;
+
+        when(categoryRepository.findCategoriesById(request)).thenReturn(Optional.empty());
+
+        assertThrows(ResourceNotFoundException.class, () -> categoryService.deleteCategory(request));
+
+        verify(categoryRepository, never()).deleteById(request);
+
+    }
+
+    //Helper methods
+    private Category createCategory(Integer id, String name){
+        Category category = new Category();
+        category.setName(name);
+        category.setId(id);
+        return category;
+    }
+
+    private CategoryDTO createDTO(Integer id, String name) {
+        CategoryDTO dto = new CategoryDTO();
+        dto.setId(id);
+        dto.setName(name);
+        return dto;
     }
 }
